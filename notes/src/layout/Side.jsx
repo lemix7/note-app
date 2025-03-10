6aeada2df2b8c531d08e419dc0008742add33b26
Ahmad <ahmedgeger@gmail.com>
2025-03-11 00:15:58 +0200

    removing favicon

--- notes/src/layout/Side.jsx
+++ notes/src/layout/Side.jsx
@@ -3,8 +3,6 @@ import AddNoteBtn from "../components/AddNoteBtn";
 import Logo from "../components/Logo";
 
 const Side = () => {
-
-  
   return (
     <div className="w-[30%] bg-black lg:w-[20%] h-screen border-r border-gray-400 flex flex-col items-center gap-4">
       <Logo />
