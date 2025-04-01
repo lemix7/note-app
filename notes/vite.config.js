cb10a6b8d82b2c125d0b8f8ffdb38a142ba9ea51
Ahmad <ahmedgeger@gmail.com>
2025-04-02 00:13:34 +0300

    base modify

--- notes/vite.config.js
+++ notes/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: process.env.VITE_BASE_PATH || '/note-app',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
