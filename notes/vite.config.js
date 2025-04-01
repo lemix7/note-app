633047f79feeb9a280f971dd04f15b06d66118f1
Ahmad <ahmedgeger@gmail.com>
2025-04-02 00:21:32 +0300

    vercel json

--- notes/vite.config.js
+++ notes/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: "./",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
   },
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
